bbc37573956de3054041e1b301e99204567fd11d
gaboogled <gaboogled@gmail.com>
2019-03-30 05:39:33 -0700

    Update dependencies.php

--- mvc/dependencies.php
+++ mvc/dependencies.php
@@ -1,2 +1,50 @@
 <?php
 // DI Configurations based on shameerc/slim3-aura-di
+
+
+// DIC configuration
+$container = $app->getContainer();
+// -----------------------------------------------------------------------------
+// Service providers
+// -----------------------------------------------------------------------------
+
+// PDO database library
+$container['db'] = function ($c) {
+    $settings = $c->get('settings')['db'];
+    $pdo = new PDO("mysql:host=" . $settings['host'] . ";dbname=" . $settings['dbname'],
+        $settings['user'], $settings['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+/* Twig
+$container['view'] = function ($c) {
+    $settings = $c->get('settings');
+    $view = new Slim\Views\Twig($settings['view']['template_path'], $settings['view']['twig']);
+    // Add extensions
+    $view->addExtension(new Slim\Views\TwigExtension($c->get('router'), $c->get('request')->getUri()));
+    $view->addExtension(new Twig_Extension_Debug());
+    return $view;
+};
+
+// Flash messages
+$container['flash'] = function ($c) {
+    return new Slim\Flash\Messages;
+};
+
+// -----------------------------------------------------------------------------
+// Service factories
+// -----------------------------------------------------------------------------
+// monolog
+$container['logger'] = function ($c) {
+    $settings = $c->get('settings');
+    $logger = new Monolog\Logger($settings['logger']['name']);
+    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['logger']['path'], Monolog\Logger::DEBUG));
+    return $logger;
+}; */
+
+// -----------------------------------------------------------------------------
+// Action factories
+// -----------------------------------------------------------------------------
