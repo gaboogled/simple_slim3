e2c17ab76677f3fff937f2f1d962c8f72e0fb1aa
gaboogled <gaboogled@gmail.com>
2019-03-30 05:29:56 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,19 @@
 <?php
 
-$app = new \Slim\App(['settings' => ['displayErrorDetails' => true]]);
+
 
 date_default_timezone_set('America/Arizona');
+
+
+$config = [
+    'settings' => [
+        'displayErrorDetails' => true,
+
+        'logger' => [
+            'name' => 'slim3mvc',
+            'level' => Monolog\Logger::DEBUG,
+            'path' => __DIR__ . '/logs/slim3mvc.log',
+        ],
+    ],
+];
+$app = new \Slim\App($config);
