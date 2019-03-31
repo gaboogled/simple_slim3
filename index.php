24cdc5942225f828bbf1c60fed2a780df6b0c55d
gaboogled <gaboogled@gmail.com>
2019-03-31 13:21:56 -0700

    Update

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * simple Slim 3 MVC Framework (https://github.com/gaboogled/simple)
+ *
+ * @link      https://github.com/slimphp/Slim
+ * @copyright Copyright (c) 2019 George Freeney-Jones
+ * @license   https://github.com/gaboogled/simple/blob/master/LICENSE/ (MIT License)
+ */
+
+
 /* Slim 3 is the Core Application. This is the entry point and a simple MVC Base layer to the simple home page (_templates/index.html). */
 
 require '_config/mvc_app_config.php'; // Defalt simple Application settings.
