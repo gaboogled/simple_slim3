b25a92e4e04823da96212b5a96758cac5a22843c
gaboogled <gaboogled@gmail.com>
2019-03-31 13:03:55 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <?php
-/* Slim 3 is the Core Application. This is the entry point and a simple MVC Base
- layer to the simple home page (_templates/index.html).
-Entry point to the simple application */
+/* Slim 3 is the Core Application. This is the entry point and a simple MVC Base layer to the simple home page (_templates/index.html). */
 
-require '_config/mvc_app_config.php';
-
-//Global funtionalities for All Apps, such as form validations etc.
-//------------------ Core MVC Pipeline ---------------------//
+require '_config/mvc_app_config.php'; // Defalt simple Application settings.
+//------- Nothing below this. Apps will connect in BaseRouter.php ------------//
+//       ⇣
+//------------------------- Core MVC Pipeline --------------------------------//
 include 'mvc/BaseDependencies.php';
 include 'mvc/BaseModels.php';
 include 'mvc/BaseViews.php';
 include 'mvc/BaseController.php';
 include 'mvc/BaseMiddleware.php';
-include 'mvc/BaseRouter.php'; //
-include 'mvc/BaseTest.php'; // Can place Global test cases in BaseMiddleware
-
-// Nothing else below this line,
-//----------------------------------------------------------//
+include 'mvc/BaseRouter.php';
+include 'mvc/BaseTest.php';
+//----------------------------------------------------------------------------//
+//       ⇣
+//--------- Nothing else below this line, it's simple as that! ---------------//
 $app->run();
-//It's simple as that!
