da8adff92bf81fbd62c9a77f4910a131af149853
gaboogled <gaboogled@gmail.com>
2019-03-31 11:19:25 -0700

    Bug Fixes on index.php BaseModels.php Added BaseTest.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-/* Entry point to the simple application */
+/* Slim 3 is the Core Application. This is the entry point and a simple MVC Base
+ layer to the simple home page (_templates/index.html).
+Entry point to the simple application */
 
 require '_config/mvc_app_config.php';
 
-//Slim is the Core Application. This is a simple MVC Base layer starting point.
 //This MVC pipline is good for global funtionalities such as form validations etc.
 include 'mvc/BaseDependencies.php';
 include 'mvc/BaseModels.php';
@@ -11,6 +12,7 @@ include 'mvc/BaseViews.php';
 include 'mvc/BaseController.php';
 include 'mvc/BaseMiddleware.php';
 include 'mvc/BaseRouter.php';
+include 'mvc/BaseTest.php'; // Can place Global test cases in BaseMiddleware
 
 // Run app
 $app->run();
