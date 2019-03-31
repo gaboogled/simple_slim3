c0a9f76b1f7af95ffa9216b68926f94b769ac260
gaboogled <gaboogled@gmail.com>
2019-03-31 13:22:42 -0700

    Fix

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * simple Slim 3 MVC Framework (https://github.com/gaboogled/simple)
  *
- * @link      https://github.com/slimphp/Slim
+ * @link      https://github.com/gaboogled/simple
  * @copyright Copyright (c) 2019 George Freeney-Jones
  * @license   https://github.com/gaboogled/simple/blob/master/LICENSE/ (MIT License)
  */
